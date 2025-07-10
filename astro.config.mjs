14d837cb76ed4e1707e25d20693ad67babc222c7
Garrett W. <garrettwrecovery@gmail.com>
2025-07-10 23:07:59 +0000

    a few integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+import purgecss from 'astro-purgecss';
+import playformInline from '@playform/inline';
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    // icon(), // https://github.com/natemoo-re/astro-icon || https://icon-sets.iconify.design
+    purgecss(),
+    playformInline(),
+    playformCompress(),
+  ],
+});
